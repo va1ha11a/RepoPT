7b8e060b0c070f47784ae63cabbde885e16f3394
Rob K <rjkershaw@gmail.com>
2024-06-24 09:01:15 +0000

    add some more examples

--- repopt/src/main.rs
+++ repopt/src/main.rs
@@ -16,10 +16,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Use the deserialized data (example)
 
+    println!("Project one Open Tickets:");
     let tiks: Vec<_> = in_repo_db
         .iter_tickets()
         .filter(|t| t.get_project_id() == "P0001")
         .filter(|t| t.is_open())
+        .collect();
+    println!("{:#?}", tiks);
+
+    println!("Project two Closed Tickets:");
+    let tiks: Vec<_> = in_repo_db
+        .iter_tickets()
+        .filter(|t| t.get_project_id() == "P0002")
+        .filter(|t| !t.is_open())
         .collect();
     println!("{:#?}", tiks);
 
