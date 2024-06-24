50ae1b30d0e24b7cab5e107510866640adf22568
Rob K <rjkershaw@gmail.com>
2024-06-24 09:23:03 +0000

    more clean and refactor

---
+++ repopt/src/in_repo_db.rs
@@ -0,0 +1,36 @@
+use crate::in_repo_db_structs::{InRepoDB, Project, Ticket};
+use crate::toml_utils;
+
+use std::collections::HashMap;
+use std::fs;
+
+const BASE_DIR: &str = "example_data";
+const PROJECTS_DIR: &str = "projects";
+const TICKETS_DIR: &str = "tickets";
+
+use std::path::PathBuf;
+
+pub(crate) fn collect_in_repo_db() -> Result<InRepoDB, Box<dyn std::error::Error>> {
+    let project_path = PathBuf::from(BASE_DIR).join(PROJECTS_DIR);
+    let ticket_path = PathBuf::from(BASE_DIR).join(TICKETS_DIR);
+
+    let projects: HashMap<String, Project> = toml_utils::get_toml_files_in_dir(&project_path)?
+        .into_iter()
+        .map(|proj_file| -> Result<_, Box<dyn std::error::Error>> {
+            let proj_contents = fs::read_to_string(&proj_file)?;
+            let project: Project = toml::from_str(&proj_contents)?;
+            Ok((project.id.clone(), project))
+        })
+        .collect::<Result<_, _>>()?;
+
+    let tickets: HashMap<_, _> = toml_utils::get_toml_files_in_dir(&ticket_path)?
+        .into_iter()
+        .map(|ticket_file| -> Result<_, Box<dyn std::error::Error>> {
+            let ticket_contents = fs::read_to_string(&ticket_file)?;
+            let ticket: Ticket = toml::from_str(&ticket_contents)?;
+            Ok((ticket.id.clone(), ticket))
+        })
+        .collect::<Result<_, _>>()?;
+
+    Ok(InRepoDB::new(projects, tickets))
+}
--- repopt/src/main.rs
+++ repopt/src/main.rs
@@ -1,18 +1,9 @@
-use std::collections::HashMap;
-use std::fs;
-use std::path::PathBuf;
-
-mod project_data;
+mod in_repo_db;
+mod in_repo_db_structs;
 mod toml_utils;
 
-use project_data::{InRepoDB, Project, Ticket};
-
-const BASE_DIR: &str = "example_data";
-const PROJECTS_DIR: &str = "projects";
-const TICKETS_DIR: &str = "tickets";
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let in_repo_db = collect_in_repo_db()?;
+    let in_repo_db = in_repo_db::collect_in_repo_db()?;
 
     // Use the deserialized data (example)
 
@@ -33,29 +24,4 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("{:#?}", tiks);
 
     Ok(())
-}
-
-fn collect_in_repo_db() -> Result<InRepoDB, Box<dyn std::error::Error>> {
-    let project_path = PathBuf::from(BASE_DIR).join(PROJECTS_DIR);
-    let ticket_path = PathBuf::from(BASE_DIR).join(TICKETS_DIR);
-
-    let projects: HashMap<String, Project> = toml_utils::get_toml_files_in_dir(&project_path)?
-        .into_iter()
-        .map(|proj_file| -> Result<_, Box<dyn std::error::Error>> {
-            let proj_contents = fs::read_to_string(&proj_file)?;
-            let project: Project = toml::from_str(&proj_contents)?;
-            Ok((project.id.clone(), project))
-        })
-        .collect::<Result<_, _>>()?;
-
-    let tickets: HashMap<_, _> = toml_utils::get_toml_files_in_dir(&ticket_path)?
-        .into_iter()
-        .map(|ticket_file| -> Result<_, Box<dyn std::error::Error>> {
-            let ticket_contents = fs::read_to_string(&ticket_file)?;
-            let ticket: Ticket = toml::from_str(&ticket_contents)?;
-            Ok((ticket.id.clone(), ticket))
-        })
-        .collect::<Result<_, _>>()?;
-
-    Ok(InRepoDB::new(projects, tickets))
 }
