7ad639ca9451846b2f9f5a15dc5394ac1e540048
Rob K <rjkershaw@gmail.com>
2024-06-24 08:06:35 +0000

    move collection to its own function

--- repopt/src/main.rs
+++ repopt/src/main.rs
@@ -12,28 +12,7 @@ const PROJECTS_DIR: &str = "projects";
 const TICKETS_DIR: &str = "tickets";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let base_path = Path::new(BASE_DIR);
-    let project_path = base_path.join(PROJECTS_DIR);
-    let ticket_path = base_path.join(TICKETS_DIR);
-
-    let project_files = toml_utils::get_toml_files_in_dir(&project_path)?;
-    let ticket_files = toml_utils::get_toml_files_in_dir(&ticket_path)?;
-
-    let mut projects = HashMap::new();
-    for proj_file in project_files {
-        let proj_contents = fs::read_to_string(&proj_file)?;
-        let project: Project = toml::from_str(&proj_contents)?;
-        projects.insert(project.id.clone(), project);
-    }
-
-    let mut tickets = HashMap::new();
-    for ticket_file in ticket_files {
-        let ticket_contents = fs::read_to_string(&ticket_file)?;
-        let ticket: Ticket = toml::from_str(&ticket_contents)?;
-        tickets.insert(ticket.id.clone(), ticket);
-    }
-
-    let in_repo_db = InRepoDB::new(projects, tickets);
+    let in_repo_db = collect_in_repo_db()?;
 
     // Use the deserialized data (example)
 
@@ -45,4 +24,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("{:#?}", tiks);
 
     Ok(())
+}
+
+fn collect_in_repo_db() -> Result<InRepoDB, Box<dyn std::error::Error>> {
+    let base_path = Path::new(BASE_DIR);
+    let project_path = base_path.join(PROJECTS_DIR);
+    let ticket_path = base_path.join(TICKETS_DIR);
+    let project_files = toml_utils::get_toml_files_in_dir(&project_path)?;
+    let ticket_files = toml_utils::get_toml_files_in_dir(&ticket_path)?;
+    let mut projects = HashMap::new();
+    for proj_file in project_files {
+        let proj_contents = fs::read_to_string(&proj_file)?;
+        let project: Project = toml::from_str(&proj_contents)?;
+        projects.insert(project.id.clone(), project);
+    }
+    let mut tickets = HashMap::new();
+    for ticket_file in ticket_files {
+        let ticket_contents = fs::read_to_string(&ticket_file)?;
+        let ticket: Ticket = toml::from_str(&ticket_contents)?;
+        tickets.insert(ticket.id.clone(), ticket);
+    }
+    let in_repo_db = InRepoDB::new(projects, tickets);
+    Ok(in_repo_db)
 }
