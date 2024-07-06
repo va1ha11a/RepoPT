636877097f546b2d97559aa0bb7f50d0d8e3b2dc
Rob K <rjkershaw@gmail.com>
2024-07-06 22:11:13 +0000

    Add more very basic tests

--- repopt/src/in_repo_db/structs.rs
+++ repopt/src/in_repo_db/structs.rs
@@ -181,7 +181,7 @@ impl<'a, T> TicketFilters<'a> for T where T: Iterator<Item = &'a Ticket> + 'a {}
 mod tests {
     use super::*;
 
-    fn setup_in_repo_db() -> InRepoDB {
+    fn setup_in_repo_db_one() -> InRepoDB {
         let project_id = ProjectId("P0001".to_string());
         let project = Project {
             id: project_id.clone(),
@@ -210,19 +210,65 @@ mod tests {
 
     #[test]
     fn test_get_next_ticket_id() {
-        let in_repo_db = setup_in_repo_db();
-
+        let in_repo_db = setup_in_repo_db_one();
         let next_ticket_id = in_repo_db.get_next_ticket_id();
-
         assert_eq!(next_ticket_id, Some(TicketId("T0002".to_string())));
     }
 
     #[test]
     fn test_get_next_project_id() {
-        let in_repo_db = setup_in_repo_db();
-
+        let in_repo_db = setup_in_repo_db_one();
         let next_project_id = in_repo_db.get_next_project_id();
-
         assert_eq!(next_project_id, Some(ProjectId("P0002".to_string())));
+    }
+
+    #[test]
+    fn test_get_project() {
+        let in_repo_db = setup_in_repo_db_one();
+        let project_id = ProjectId("P0001".to_string());
+        let project = in_repo_db.get_project(&project_id);
+        assert_eq!(project.is_some(), true);
+    }
+
+    #[test]
+    fn test_get_ticket() {
+        let in_repo_db = setup_in_repo_db_one();
+        let ticket_id = TicketId("T0001".to_string());
+        let ticket = in_repo_db.get_ticket(&ticket_id);
+        assert_eq!(ticket.is_some(), true);
+    }
+
+    #[test]
+    fn test_iter_projects() {
+        let in_repo_db = setup_in_repo_db_one();
+        let projects: Vec<&Project> = in_repo_db.iter_projects().collect();
+        assert_eq!(projects.len(), 1);
+    }
+
+    #[test]
+    fn test_iter_tickets() {
+        let in_repo_db = setup_in_repo_db_one();
+        let tickets: Vec<&Ticket> = in_repo_db.iter_tickets().collect();
+        assert_eq!(tickets.len(), 1);
+    }
+
+    #[test]
+    fn test_with_status() {
+        let in_repo_db = setup_in_repo_db_one();
+        let open_tickets: Vec<&Ticket> = in_repo_db
+            .iter_tickets()
+            .with_status(TicketStatus::Open)
+            .collect();
+        assert_eq!(open_tickets.len(), 1);
+    }
+
+    #[test]
+    fn test_with_type() {
+        let in_repo_db = setup_in_repo_db_one();
+        let bug_tickets: Vec<&Ticket> = in_repo_db
+            .iter_tickets()
+            .with_type(TicketType::Bug)
+            .collect();
+        assert_eq!(bug_tickets.len(), 1);
     }
 }
